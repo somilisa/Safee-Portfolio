5839b79a57674465494795b6b8e71b6c6cdb6fd6
somilisa <somilisa1010@gmail.com>
2024-02-02 16:48:45 +0100

    feat: home section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="main-container">
+        <Home/>
+        <hr/>
+        <section>
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+<section>
+          <div className='back'>
+          <img src="./back2x.png" alt=""/>  
+          </div>          
+          <div className='text'>
+            <h2>Creating seamless digital journeys...</h2>
+            <div className='intro'>
+              <p>Adesina Sofiat</p>
+              <p>Product (UI/UX) Designer</p>
+              <p>adesinasafee@gmail.com</p>
+            </div>
+          </div>
+        </section>  )
+}
+
+export default Home
